769e4dfc04c2ab0fa2aded196e4f97d6c14ff136
rick <rickwestdev@gmail.com>
2018-09-07 00:46:20 +0100

    make socials configurable

--- config.php
+++ config.php
@@ -5,4 +5,18 @@ return [
     'production' => false,
     'author' => 'Rick West',
     'collections' => ['posts'],
+    'socials' => [
+        'twitter' => [
+            'link' => '#',
+            'icon' => 'fab fa-twitter',
+        ],
+        'facebook' => [
+            'link' => '#',
+            'icon' => 'fab fa-facebook-f',
+        ],
+        'github' => [
+            'link' => '#',
+            'icon' => 'fab fa-github',
+        ],
+    ]
 ];
--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -3,30 +3,16 @@
         <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
                 <ul class="list-inline text-center">
-                    <li class="list-inline-item">
-                        <a href="#">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                  </span>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                  </span>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">
-                  <span class="fa-stack fa-lg">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                  </span>
-                        </a>
-                    </li>
+                    @foreach($page->socials as $social)
+                        <li class="list-inline-item">
+                            <a href="{{ $social->link }}">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="{{ $social->icon }} fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    @endforeach
                 </ul>
                 <p class="copyright text-muted">Copyright &copy; Your Website 2018</p>
             </div>
