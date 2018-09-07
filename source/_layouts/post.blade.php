3bc52506d58e40f5364134c4c7a1c1d4cabb74f0
rick <rickwestdev@gmail.com>
2018-09-08 00:20:05 +0100

    configurable post header image

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -5,7 +5,7 @@
     @include('_partials.navigation')
 
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('../assets/images/post-bg.jpg')">
+    <header class="masthead" style="background-image: url({{ $page->image }})">
         <div class="overlay"></div>
         <div class="container">
             <div class="row">
