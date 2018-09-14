b8a5f34942c861ecfe1a931daa44a0e26bacbe23
rick <rickwestdev@gmail.com>
2018-09-14 22:03:36 +0100

    open social links in new window

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -5,7 +5,7 @@
                 <ul class="list-inline text-center">
                     @foreach($page->socials as $social)
                         <li class="list-inline-item">
-                            <a href="{{ $social->link }}">
+                            <a href="{{ $social->link }}" target="_blank">
                                 <span class="fa-stack fa-lg">
                                     <i class="fa fa-circle fa-stack-2x"></i>
                                     <i class="{{ $social->icon }} fa-stack-1x fa-inverse"></i>
